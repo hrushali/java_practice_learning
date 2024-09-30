a3d556a2e5ba3daa29f40224fa4d6692f9c388c1
hrushali <hrushalipatil2002@gmail.com>
2024-09-30 12:08:31 +0530

    commit

---
+++ datatype.java
@@ -0,0 +1,26 @@
+package primitive_datatypes;
+public class datatype {
+    static int ij;
+    static byte bya;
+    static char c;
+    static String s ;
+    static float f;
+    static long l;
+    static short sh;
+    static double d;
+    static boolean bb;
+    public static void main(String[] args) {
+      
+       
+        System.out.println("int"+ij);
+        System.out.println("byte"+bya);
+        System.out.println("char"+c); 
+        System.out.println(s); 
+        System.out.println(f);
+        System.out.println(l);
+        System.out.println(sh);
+        System.out.println(d);
+        System.out.println(bb);
+
+    }
+}
---
+++ underscor.java
@@ -0,0 +1,10 @@
+package primitive_datatypes;
+public class underscor {
+    public static void main(String[] args) {
+        byte var = 0b11_10;
+        int var1 = 4_5000;
+        System.out.println(var);
+        System.out.println(var1);
+    }
+    
+}
